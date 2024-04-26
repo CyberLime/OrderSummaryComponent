ee532a2a89f807a2f07d93969e2c85f8753eb9e4
CyberLime <togrul2006@inbox.ru>
2024-04-26 12:39:38 +0400

    i

--- docs/my-app/server/main.server.mjs
+++ docs/my-app/server/main.server.mjs
@@ -1,10 +1,2 @@
 import './polyfills.server.mjs';
-import {
-  main_server_default
-} from "./chunk-MGIGK4JP.mjs";
-import "./chunk-TSJYX2G4.mjs";
-import "./chunk-24VIC3GD.mjs";
-export {
-  main_server_default as default
-};
-//# sourceMappingURL=main.server.mjs.map
+import{a}from"./chunk-CYN2ITDA.mjs";import"./chunk-TLVVXGVD.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
